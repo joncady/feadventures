abefa93b7b7c20242bac3a1633f8368828669a3d
notjondo <jonathando24@yahoo.com>
2019-01-07 20:34:07 -0800

    Update enemy.js
    modified enemy movement to put it inside enemy method

--- enemy.js
+++ enemy.js
@@ -70,8 +70,46 @@ class Enemy extends Character {
         }
     }
 
-    moveEnemy(x, y) {
-        super.move(x, y);
+    moveEnemy() {
+		let newX = this.x;
+		let newY = this.y;
+		let closestPlayer;
+		let closestDistance;
+		playerList.forEach((player) => {
+			let checkPlayerVals = player.getLocation();
+			let checkX = checkPlayerVals.x;
+			let checkY = checkPlayerVals.y;
+			
+			let distance = distanceCalculation(this.x, this.y, checkX, checkY);
+			
+			if (closestPlayer == null || distance < closestDistance) {
+				closestPlayer = player;
+				closestDistance = distance;
+			}
+		});
+		
+		let playerXY = closestPlayer.getLocation();
+		let { diffX, diffY } = differenceXY(newX, newY, playerXY.x, playerXY.y);
+        let angle = returnAngle(newX, newY, playerXY.x, playerXY.y);
+        let xAngle = Math.cos(angle);
+        let yAngle = Math.sin(angle);
+        if (diffX < 0) {
+            xAngle *= -1;
+            yAngle *= -1;
+        }
+        let reverse = 1;
+        if (this.type === "archer") {
+            reverse = -1;
+        }
+        let randomPattern = getRandomInt(10);
+        if (randomPattern > 6) {
+            this.attackPlayer(xAngle, yAngle, angle);
+        } else {
+            newX = newX + this.speed * xAngle * reverse;
+            newY = newY + this.speed * yAngle * reverse;
+            this.choosePicture(xAngle, yAngle, this.el);
+        }
+        super.move(newX, newY);
     }
 
     getReference() {
